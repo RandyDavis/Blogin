8c7fc56b25b237644b631010a937174d5a18e03e
Randy Davis <randy_davis@outlook.com>
2014-10-08 15:23:30 -0500

    Create new and edit form, main views, and update the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,33 @@
 class PostsController < ApplicationController
   respond_to :html
-  
+
   def index
     @posts = Post.order("created_at desc")
     respond_with @posts
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end 
 end
